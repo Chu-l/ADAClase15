f174b3c8f72d233615108ee35a7f68e6413e7838
Lilen <lilen.chu@hotmail.com>
2026-01-10 11:40:06 -0300

    Ejercicio 9 terminado

--- actividades.js
+++ actividades.js
@@ -56,6 +56,14 @@ if (comidas.includes("Sushi")) {
 */
 /*** Ejercicio 8: Extraer una parte de una frase
 Tienes la frase "Los gatos son geniales". Extrae y muestra solo la parte "gatos" utilizando el método adecuado. ***/
-
+/*
 let frase = "Los gatos son geniales";
 console.log(frase.slice(4,9));
+*/
+/*** Ejercicio 9: Eliminar espacios innecesarios
+Tienes la siguiente cadena con espacios adicionales 
+"    Hola, mundo!    "
+Elimina los espacios al inicio y al final y muestra la cadena resultante. ***/
+
+let frase = "    Hola, mundo!    ";
+console.log(frase.trim());
